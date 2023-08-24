8a4221bcae911ed4e447ad87ff98b36c300ab4bf
bm0918 <benton.mize@dexcom.com>
2023-08-24 16:30:30 -0400

    randomize the rainbow entirely

--- rainbow.cpp
+++ rainbow.cpp
@@ -2,17 +2,18 @@
 #include "ring.h"
 
 Color colors[] = {
-    {255, 0, 0},        // red
-    {255, 128, 0},      // orange
-    {255, 255, 0},      // yellow
-    {128, 255, 0},      // light green
-    {0, 255, 0},        // green
-    {255, 128, 0},      // teal
-    {0, 255, 255},      // marine
-    {0, 0, 255},        // blue
-    {127, 0, 255},      // purple
-    {255, 0, 255},      // magenta
-    {255, 0, 127},      // pink
+    {255, 0, 0},        // red 0
+    {255, 128, 0},      // orange 1
+    {255, 255, 0},      // yellow 2
+    {128, 255, 0},      // light green 3
+    {0, 255, 0},        // green 4
+    {255, 128, 0},      // teal 5
+    {0, 255, 255},      // marine 6
+    {0, 0, 255},        // blue 7
+    {127, 0, 255},      // purple 8
+    {255, 0, 255},      // magenta 9
+    {255, 0, 127},      // pink 10
+    {255, 255, 255},    // white 11
 };
 
 uint32_t getRandomColor(Adafruit_NeoPixel& ring) {
@@ -45,15 +46,13 @@ void fire(Adafruit_NeoPixel&ring) {
     ring.setPixelColor(i, getFireColor(ring));
   }
 
-  // ring.fill(getFireColor(ring), 0, ring.numPixels());
-
   ring.setBrightness(random(8, 32));
   ring.show();
   delay(100);
 }
 
 void pulseColor(Adafruit_NeoPixel& ring, Pulse& pulse) {
-  Color c = colors[0];
+  Color c = colors[4];
 
   ring.fill(ring.Color(c.r, c.g, c.b), 0, ring.numPixels());
 
@@ -68,11 +67,12 @@ void pulseColor(Adafruit_NeoPixel& ring, Pulse& pulse) {
 void rainbow(Adafruit_NeoPixel& ring) {
   ring.setBrightness(BRIGHTNESS);
 
-  ring.fill(getRandomColor(ring), 0, ring.numPixels());
+  // ring.fill(getRandomColor(ring), 0, ring.numPixels());
+  ring.fill(ring.Color(random(0, 255), random(0, 255), random(0, 255)), 0, ring.numPixels());
 
   ring.show();
 
-  delay(250);
+  delay(200);
 }
 
 void rainbowSpin(Adafruit_NeoPixel& ring) {
